8f7067c6414fadf2e216df5286b484d19bb483d9
thomasvds <vanderstraeten.thomas@gmail.com>
2018-04-17 17:43:41 +0200

    Finalize front

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   #app
     h1 Congrats, you solved the challenge!
     p The secret is {{ secret }}.
+    p The smart contract lives at address {{ contractAddress }}
     p You can now claim your bounty by clicking on the below button. The bounty will be sent to the Ethereum address from which you will confirm the transaction.
     p A little sum of Ether is required to emit the transaction. Don't worry, the bounty is worth much more than that ;)
     p Make sure MetaMask is open and connected to the main Ethereum network.
-    button(@click="claimBounty()") Claim the bounty!
+    button(@click="claimBounty()") CLAIM YOUR BOUNTY
+    p(v-if="success") Bounty sent to your address, check this tx hash on Etherscan: {{ txHash }}.
+    p Please DO SHARE the Cryptorun.brussels love! What would be wonderful is a selfie of you with the QR code, tagging BeCode and Thomas in your post :)
 </template>
 
 <script>
@@ -16,15 +19,21 @@ export default {
   name: 'app',
   data () {
     return {
-      secret: ''
+      secret: '',
+      contractAddress: address,
+      txHash: '',
+      success: false
     }
   },
   methods: {
     claimBounty() {
+      self = this;
       if (typeof web3 !== 'undefined') {
         web3.eth.contract(abi).at(address).claimBounty(this.secret, function(error, result){
           console.log('Error: ' + error);
           console.log('Success: ' + result);
+          self.success = true;
+          self.txHash = result;
           alert('Success! Enjoy, and please share the cryptorun.brussels love ;)');
         })
       } else {
@@ -62,5 +71,17 @@ export default {
 
 a {
   color: #42b983;
+}
+
+button {
+  border-radius: 3px;
+  padding: 0.5rem 0.5rem;
+  margin: 0.5rem 1rem;
+  width: 13rem;
+  background: transparent;
+  color: #2c3e50;
+  font-size: 16px;
+  font-weight: bold;
+  border: 2px solid #2c3e50;
 }
 </style>
--- src/contract/abi.js
+++ src/contract/abi.js
@@ -1,1 +1,46 @@
-export const abi = [ ]
+export const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_secret",
+				"type": "string"
+			}
+		],
+		"name": "claimBounty",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "fallback"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_hashedSecret",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "hashedSecret",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bytes32"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
